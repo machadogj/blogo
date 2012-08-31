912716ee5d271794d5580064722ee9fe665c36ed
Gustavo Machado <machadogj@gmail.com>
2012-08-31 19:18:43 -0300

    adding public files.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var connect = require('connect'),
 	http    = require('http'),
 	path	= require('path');
 
-app = connect().use(connect["static"](path.join(process.cwd(), 'output')));
+app = connect()
+	.use(connect["static"](path.join(process.cwd(), 'public')))
+	.use(connect["static"](path.join(process.cwd(), 'output')));
 var port = process.env.PORT || 5000;
 http.createServer(app).listen(port);
 console.log("started server on http://localhost:" + port);
